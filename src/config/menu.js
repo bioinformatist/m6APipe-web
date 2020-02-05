3f7a696e5e7b2d2b65366ddb1f252ae8b1060cad
Yu Sun <suny226@mail2.sysu.edu.cn>
2020-02-05 15:56:03 +0800

    use new icon for analysis

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,12 +1,28 @@
 // 菜单配置
 // headerMenuConfig：头部导航配置
 // asideMenuConfig：侧边导航配置
+import React, { Component } from 'react';
+import DynamicIcon from '@icedesign/dynamic-icon';
+
+const CustomIcon = DynamicIcon.create({
+  fontFamily: 'iconfont',
+  prefix: 'icon',
+  css: '//at.alicdn.com/t/font_1627357_9hn4j8a8wq4.css',
+});
+
+class IconAnalysis extends Component {
+  state = {};
+
+  render() {
+    return <CustomIcon type="analysis" />;
+  }
+}
 
 const asideMenuConfig = [
   {
     name: 'Run m6APipe',
     path: '/dashboard',
-    icon: 'loading',
+    icon: <IconAnalysis />,
     children: [
       {
         name: 'Setting Parameter',
